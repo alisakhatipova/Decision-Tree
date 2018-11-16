eadc42422f78537702157a2f2de1c9410a0521b4
alisa <hatipova_a@mail.ru>
2018-11-16 15:12:03 +0100

    Imrove visualisation

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pydot
 import numpy as np
-from math import log
 
+# node id for visualization
 global_node_count = 0
 
 
@@ -9,25 +9,25 @@ def inform_gain(attrs, classes, value):
     """
     Calculate information gain
     """
-    result = entropy(attrs, classes)
+    result = entropy(classes)
     result -= cond_entropy(attrs, classes, value)
     return result
 
 
-def entropy(attrs, classes):
+def entropy(classes):
     """
     Calculate entropy
     """
-    total = len(attrs)
-    pos = len(attrs[np.where(classes == 1)])
-    neg = len(attrs[np.where(classes == 0)])
+    total = len(classes)
+    pos = len(classes[np.where(classes == 1)])
+    neg = len(classes[np.where(classes == 0)])
     result = 0.0
     if pos > 0:
         pos = float(pos) / total
-        result += pos*log(1.0 / pos, 2)
+        result += pos * np.log2(1.0 / pos)
     if neg > 0:
         neg = float(neg) / total
-        result += neg * log(1.0 / neg, 2)
+        result += neg * np.log2(1.0 / neg)
     return result
 
 
@@ -35,13 +35,13 @@ def cond_entropy(attrs, classes, value):
     """
     Calculate conditional entropy
     """
-    total = len(attrs)
+    total = len(classes)
     left_split = attrs[np.where(attrs < value)]
     right_split = attrs[np.where(attrs >= value)]
     num_left = len(left_split)
     num_right = len(right_split)
-    left_entropy = entropy(left_split, classes[np.where(attrs < value)])
-    right_entropy = entropy(right_split, classes[np.where(attrs >= value)])
+    left_entropy = entropy(classes[np.where(attrs < value)])
+    right_entropy = entropy(classes[np.where(attrs >= value)])
     result = (left_entropy * float(num_left) + right_entropy * float(num_right)) / total
     return result
 
@@ -117,7 +117,7 @@ def TDIDT(data, node):
     TDIDT(right_data, node['right_child'])
 
 
-def add_node(graph, node, parent):
+def add_node(graph, node, parent, label):
     """
     Add node to the graph
     """
@@ -128,7 +128,7 @@ def add_node(graph, node, parent):
     pos = str(pos)
     samples_info = total + ' samples\n' + neg + ' of class 0, ' + pos + ' of class 1'
     if 'final_class' in node:
-        legend = str(node['id']) + '. final class is ' + str(node['final_class']) + '\n' + samples_info
+        legend = str(node['id']) + '. final class is ' + str(node['final_class'])
         new_node = pydot.Node(legend)
     else:
         legend = str(node['id']) + '. ' + node['split_attr'] + \
@@ -136,11 +136,11 @@ def add_node(graph, node, parent):
         new_node = pydot.Node(legend)
     graph.add_node(new_node)
     if parent:
-        graph.add_edge(pydot.Edge(parent, new_node))
+        graph.add_edge(pydot.Edge(parent, new_node, label=str(label),labelfontcolor="#009933", fontsize="10.0", color="blue"))
     if 'left_child' in node:
-        add_node(graph, node['left_child'], new_node)
+        add_node(graph, node['left_child'], new_node, True)
     if 'right_child' in node:
-        add_node(graph, node['right_child'], new_node)
+        add_node(graph, node['right_child'], new_node, False)
 
 
 def save_dot(tree):
@@ -148,8 +148,19 @@ def save_dot(tree):
     Visualize the resulting decision tree
     """
     graph = pydot.Dot(graph_type='graph')
-    add_node(graph, tree, None)
+    add_node(graph, tree, None, None)
     graph.write_png('out_graph.png')
+
+
+def predict(node, data):
+    if 'final_class' in node:
+        return node['final_class']
+    attr_val = data[node['split_attr']]
+    split_value = node['split_value']
+    if attr_val < split_value:
+        return predict(node['left_child'], data)
+    else:
+        return predict(node['right_child'], data)
 
 
 if __name__ == "__main__":
@@ -157,5 +168,14 @@ if __name__ == "__main__":
     data = np.genfromtxt(path_to_csv, dtype=float, delimiter=',', names=True)
     tree = {}
     TDIDT(data, tree)
-    print(tree)
     save_dot(tree)
+    path_to_test_csv = "gene_expression_test.csv"
+    data = np.genfromtxt(path_to_test_csv, dtype=float, delimiter=',', names=True)
+    total = len(data)
+    correctly_classified = 0
+    for val in data:
+        predicted = predict(tree, val)
+        real = val["class_label"].astype(int)
+        if predicted == real:
+            correctly_classified +=1
+    print(str(float(correctly_classified) / total) + ' correctly classified')
